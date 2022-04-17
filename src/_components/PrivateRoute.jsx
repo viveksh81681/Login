80405455c98f61a4e33b3975e68c39c0567185f7
viveksh81681 <viveksh81681@gmail.com>
2022-04-18 03:51:14 +0530

    Update

---
+++ src/_components/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Navigate } from "react-router-dom";
+
+export const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      localStorage.getItem("user") ? (
+        <Component {...props} />
+      ) : (
+        <Navigate
+          to={{ pathname: "/login", state: { from: props.location } }}
+        />
+      )
+    }
+  />
+);
